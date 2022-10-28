df7cca71a3cb6cfce0d8fa977c0e93732fb7edf5
임나현 <nahyun9811@naver.com>
2022-10-28 19:14:23 +0900

    Update

--- LottoExpress/MainWindow.xaml.cs
+++ LottoExpress/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LottoExpress;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,3 +32,5 @@ namespace LottoExpress
         }
     }
 }
+
+
